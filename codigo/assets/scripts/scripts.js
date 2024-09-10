d40c7fda30562b8b6b08ab66bb65c4ba8afdad56
Stambassi <augustostambassiduarte@gmail.com>
2024-09-09 22:08:54 -0300

    conflito de merge

--- codigo/assets/scripts/scripts.js
+++ codigo/assets/scripts/scripts.js
@@ -1,183 +1,4 @@
 
-function getAnimalGenero(){
-  let genero_form = document.getElementsByName('a_genero');
-  let genero_value;
-  
-  genero_form.forEach(element => {
-    if(element.checked == true){
-      genero_value = element.value;
-    }
-  });
-
-  return genero_value;
-}
-
-function getAnimalCastrado(){
-  let castrado_form = document.getElementsByName('a_castrado');
-  let castrado_value;
-  
-  castrado_form.forEach(element => {
-    if(element.checked == true){
-      if(element.value === "true"){
-        castrado_value = true;
-      } else {
-        castrado_value = false;
-      }
-    }
-  });
-  return castrado_value;
-}
-
-function getAnimalEspecie(){
-  let especie_form = document.getElementsByName('a_especie');
-  let especie_outros = document.getElementById('outros_value').value;
-  let especie_value;
-
-  especie_form.forEach(element => {
-    if(element.checked == true){
-      if(element.value === "outros"){
-        especie_value = especie_outros;
-      } else {
-        especie_value = element.value;
-      }
-    }
-  });
-
-  return especie_value;
-}
-
-function getAnimalVacina(){
-  let vacina_form = document.getElementsByName('a_vacina');
-  const vacina_value = [];
-  vacina_form.forEach(element => {
-    if(element.checked == true){
-      vacina_value.push(element.value);
-    }
-  });
-  return vacina_value;
-}
-
-function changePageAnimal(page){
-
-  let divFirstPage = document.getElementById('animal-form-1');
-  let divSecondPage = document.getElementById('animal-form-2');
-  let stageOne = document.getElementById('Cadastro-geral');
-  let stageTwo = document.getElementById('Cadastro-especifico');
-  let next = document.getElementById('btn-animal-passar');
-  let back = document.getElementById('btn-animal-voltar');
-  let nome_holder = document.getElementsByClassName('animal_name');
-  let artigo_holder = document.getElementsByClassName('animal_artigo');
-
-  let name =  document.getElementById('a_name').value;
-  let data_nascimento = document.getElementById('a_data').value;
-  let foto
-
-  
-  let Raca = document.getElementById('a_raca').value;
-  let bio = document.getElementById('a_bio').value;
-
-
-  switch (page) {
-    case "Pagina1":
-      // muda o form que esta aparecendo
-      divFirstPage.style.display = "block";
-      divSecondPage.style.display = "none";
-
-      // atualiza os botoes
-      next.innerText = "Próximo";
-      next.setAttribute("onClick","javascript: changePageAnimal('Pagina2')");
-      back.style.display = "none";
-
-      // muda a barra de progresso
-      stageTwo.style.background = "white";
-      break;
-
-    case "Pagina2":
-      let genero_value = getAnimalGenero();      
-      let especie_value = getAnimalEspecie();
-
-      for(let i = 0; i<nome_holder.length;i++){
-        nome_holder[i].innerText = name;
-      }
-      console.log(artigo_holder);
-
-      for(let i = 0; i<artigo_holder.length;i++){
-        if(genero_value == 'F'){
-          artigo_holder[i].innerText = 'a';
-        } else {
-          artigo_holder[i].innerText = 'o';
-        }
-      }
-
-      console.log(data_nascimento);
-      if(name === "" || data_nascimento === "" || especie_value === ""){ // condicao para mudar de pagina
-        alert("erro");
-      } else { // mensagem de erro
-        
-        // muda o form que esta aparecendo
-        divFirstPage.style.display = "none";
-        divSecondPage.style.display = "block";
-
-        // atualiza os botoes
-        next.innerText = "Finalizar";
-        next.setAttribute("onClick","javascript: changePageAnimal('Finalizar')");
-        back.style.display = "block";
-
-        // muda a barra de progresso
-        stageTwo.style.background = "var(--cor_primaria)";
-      }
-      break;
-    case "Finalizar": 
-      if(bio <= 1){ // condicao para finalizar (somente com a bio preenchida pode finalizar)
-        alert("erro"); 
-      } else {
-        let especie_value = getAnimalEspecie();
-        let genero_value = getAnimalGenero();
-        let isCastrado = getAnimalCastrado();
-        let vacina_value = getAnimalVacina();
-
-        let animal_data = {
-          nome: name,
-          genero: genero_value,
-          especie: especie_value,
-          dt_nascimento: data_nascimento,
-          castrado: isCastrado,
-          //foto_animal: foto,
-          raca: Raca,
-          historia: bio,
-          vacina: vacina_value,
-        }
-        saveAnimalData(animal_data);
-      }
-      
-      break;
-  }
-}
-
-function saveAnimalData(data){
-  console.log(data);
-}
-
-/* Definir comportamento ao clicar em outros no cadastro do animal */
-
-let especieAnimal = document.querySelectorAll('input[name=a_especie]');
-especieAnimal.forEach(element => {
-  element.addEventListener("click" , () =>{
-      let div = document.getElementById("outros_value");
-      if(element.checked){
-        if(element.value == "outros"){
-          div.style.display = "inline-block"
-        } else {
-          div.style.display = "none"
-        }
-      } 
-    });
-  });
-function mostrarErro (mensagem)
-{
-  console.log(mensagem);
-}
-
 
 
 /* ------------------------ Definir comportamento para mostrar o pop-up de LOGIN (INICIO) ------------------------ */
@@ -655,3 +476,187 @@ function postUsuario (objUsuario)
 {
   console.log(objUsuario);
 }
+
+
+/* --------------------- Definir comportamento do CADASTRO DE ANIMAL (INICIO) ------------------------ */
+
+function getAnimalGenero(){
+  let genero_form = document.getElementsByName('a_genero');
+  let genero_value;
+  
+  genero_form.forEach(element => {
+    if(element.checked == true){
+      genero_value = element.value;
+    }
+  });
+
+  return genero_value;
+}
+
+function getAnimalCastrado(){
+  let castrado_form = document.getElementsByName('a_castrado');
+  let castrado_value;
+  
+  castrado_form.forEach(element => {
+    if(element.checked == true){
+      if(element.value === "true"){
+        castrado_value = true;
+      } else {
+        castrado_value = false;
+      }
+    }
+  });
+  return castrado_value;
+}
+
+function getAnimalEspecie(){
+  let especie_form = document.getElementsByName('a_especie');
+  let especie_outros = document.getElementById('outros_value').value;
+  let especie_value;
+
+  especie_form.forEach(element => {
+    if(element.checked == true){
+      if(element.value === "outros"){
+        especie_value = especie_outros;
+      } else {
+        especie_value = element.value;
+      }
+    }
+  });
+
+  return especie_value;
+}
+
+function getAnimalVacina(){
+  let vacina_form = document.getElementsByName('a_vacina');
+  const vacina_value = [];
+  vacina_form.forEach(element => {
+    if(element.checked == true){
+      vacina_value.push(element.value);
+    }
+  });
+  return vacina_value;
+}
+
+function changePageAnimal(page){
+
+  let divFirstPage = document.getElementById('animal-form-1');
+  let divSecondPage = document.getElementById('animal-form-2');
+  let stageOne = document.getElementById('Cadastro-geral');
+  let stageTwo = document.getElementById('Cadastro-especifico');
+  let next = document.getElementById('btn-animal-passar');
+  let back = document.getElementById('btn-animal-voltar');
+  let nome_holder = document.getElementsByClassName('animal_name');
+  let artigo_holder = document.getElementsByClassName('animal_artigo');
+
+  let name =  document.getElementById('a_name').value;
+  let data_nascimento = document.getElementById('a_data').value;
+  let foto
+
+  
+  let Raca = document.getElementById('a_raca').value;
+  let bio = document.getElementById('a_bio').value;
+
+
+  switch (page) {
+    case "Pagina1":
+      // muda o form que esta aparecendo
+      divFirstPage.style.display = "block";
+      divSecondPage.style.display = "none";
+
+      // atualiza os botoes
+      next.innerText = "Próximo";
+      next.setAttribute("onClick","javascript: changePageAnimal('Pagina2')");
+      back.style.display = "none";
+
+      // muda a barra de progresso
+      stageTwo.style.background = "white";
+      break;
+
+    case "Pagina2":
+      let genero_value = getAnimalGenero();      
+      let especie_value = getAnimalEspecie();
+
+      for(let i = 0; i<nome_holder.length;i++){
+        nome_holder[i].innerText = name;
+      }
+      console.log(artigo_holder);
+
+      for(let i = 0; i<artigo_holder.length;i++){
+        if(genero_value == 'F'){
+          artigo_holder[i].innerText = 'a';
+        } else {
+          artigo_holder[i].innerText = 'o';
+        }
+      }
+
+      console.log(data_nascimento);
+      if(name === "" || data_nascimento === "" || especie_value === ""){ // condicao para mudar de pagina
+        alert("erro");
+      } else { // mensagem de erro
+        
+        // muda o form que esta aparecendo
+        divFirstPage.style.display = "none";
+        divSecondPage.style.display = "block";
+
+        // atualiza os botoes
+        next.innerText = "Finalizar";
+        next.setAttribute("onClick","javascript: changePageAnimal('Finalizar')");
+        back.style.display = "block";
+
+        // muda a barra de progresso
+        stageTwo.style.background = "var(--cor_primaria)";
+      }
+      break;
+    case "Finalizar": 
+      if(bio <= 1){ // condicao para finalizar (somente com a bio preenchida pode finalizar)
+        alert("erro"); 
+      } else {
+        let especie_value = getAnimalEspecie();
+        let genero_value = getAnimalGenero();
+        let isCastrado = getAnimalCastrado();
+        let vacina_value = getAnimalVacina();
+
+        let animal_data = {
+          nome: name,
+          genero: genero_value,
+          especie: especie_value,
+          dt_nascimento: data_nascimento,
+          castrado: isCastrado,
+          //foto_animal: foto,
+          raca: Raca,
+          historia: bio,
+          vacina: vacina_value,
+        }
+        saveAnimalData(animal_data);
+      }
+      
+      break;
+  }
+}
+
+function saveAnimalData(data){
+  console.log(data);
+}
+
+/* Definir comportamento ao clicar em outros no cadastro do animal */
+
+let especieAnimal = document.querySelectorAll('input[name=a_especie]');
+especieAnimal.forEach(element => {
+  element.addEventListener("click" , () =>{
+      let div = document.getElementById("outros_value");
+      if(element.checked){
+        if(element.value == "outros"){
+          div.style.display = "inline-block"
+        } else {
+          div.style.display = "none"
+        }
+      } 
+    });
+  });
+function mostrarErro (mensagem)
+{
+  console.log(mensagem);
+}
+
+/* --------------------- Definir comportamento do CADASTRO DE ANIMAL (FIM) ------------------------ */
